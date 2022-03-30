c04cabfdbeda0926e61345c2f630ecd906d55002
paulspam <paulspam@ukr.net>
2022-03-30 18:02:35 +0300

    Adding tests (UserController.*, no authentication)

--- src/main/java/com/softserveinc/ita/javaclub/volleyblog/model/User.java
+++ src/main/java/com/softserveinc/ita/javaclub/volleyblog/model/User.java
@@ -1,9 +1,6 @@
 package com.softserveinc.ita.javaclub.volleyblog.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -11,6 +8,7 @@ import javax.persistence.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 public class User {
